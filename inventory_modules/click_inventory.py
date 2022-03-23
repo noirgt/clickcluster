5e46c5620d49a373cbbc45f2760d47a02aa58a0c
Gennadiy Tokarev <gtokarev@innoseti.ru>
2022-03-23 09:36:12 +0200

    Move files for inventory

--- inventory_modules/click_inventory.py
+++ inventory_modules/click_inventory.py
@@ -1,11 +1,10 @@
 from json import dumps as convert_to_json
 
-from tenacity import retry_if_exception
-
 def click_inventory(click_cluster):
     shards_dict = {}
     zoo_structure_list = []
     distr_structure_list = []
+    full_distr_cluster = {}
 
     # Added click_containers
     for id_shard in click_cluster:
@@ -22,21 +21,30 @@ def click_inventory(click_cluster):
                 (id_shard, f"ch{id_shard}_rep{ip_addr_id + 1}")
                 )
 
-    # Added zoo_containers
-    for ip_addr_id, ip_addr in enumerate(click_cluster.values()):
-        shards_dict[ip_addr[0]]["zoo_containers"].append(
+    # Added zoo_structure for docker-compose and config-file
+    for ip_addr_id, ip_addr in enumerate(list(shards_dict.keys())):
+        len_click_containers =  len(shards_dict[ip_addr]["click_containers"])
+        shards_dict[ip_addr]["zoo_containers"].append(
             (ip_addr_id + 1, f"zk{ip_addr_id + 1}")
         )
         zoo_structure_list.append(
             (ip_addr_id + 1, f"zk{ip_addr_id + 1}")
         )
 
-    # Added extra_hosts
+        if len_click_containers > 2:
+            shards_dict[ip_addr]["zoo_containers"].append(
+                (ip_addr_id + 1, f"zk{ip_addr_id + 1}_2")
+            )
+            zoo_structure_list.append(
+                (ip_addr_id + 1, f"zk{ip_addr_id + 1}_2")
+            )
+
+    # Added extra_hosts for docker-compose
     for ip_addr in shards_dict.keys():
         for ip_addr_other in shards_dict.keys():
             if ip_addr == ip_addr_other:
                 continue
-    
+
             for host in shards_dict[ip_addr_other]["click_containers"]:
                 shards_dict[ip_addr]["extra_hosts"].append(
                     (f"{host[1]}:{ip_addr_other}")
@@ -47,32 +55,42 @@ def click_inventory(click_cluster):
                     (f"{host[1]}:{ip_addr_other}")
                 )
 
-    # Added zoo_servers
+    # Added zoo_servers for docker-compose
     for ip_addr in shards_dict.keys():
         zk_name = shards_dict[ip_addr]["zoo_containers"][0][1]
-        shards_dict[ip_addr]["zoo_servers"] = ""
+        shards_dict[ip_addr]["zoo_servers"] = []
         
-        for zoo_container in zoo_structure_list:
-            zoo_container = zoo_container[1]
-            if zoo_container == zk_name:
-                zoo_container = "0.0.0.0"
-            shards_dict[ip_addr]["zoo_servers"] += f"{zoo_container}:2888:3888,"
-        
-        shards_dict[ip_addr]["zoo_servers"] = shards_dict[ip_addr]["zoo_servers"].rstrip(',')
+        for zk_name in shards_dict[ip_addr]["zoo_containers"]:
+            zoo_servers = ""
+            for zoo_container in zoo_structure_list:
+                zoo_container = zoo_container[1]
+                if zoo_container == zk_name[1]:
+                    zoo_container = "0.0.0.0"
+                zoo_servers += f"{zoo_container}:2888:3888,"
+            zoo_servers = zoo_servers.rstrip(',')
+            shards_dict[ip_addr]["zoo_servers"].append(zoo_servers)
 
-    # Added distr_structure
+    # Added distr_structure for docker-compose
     for ip_addr in shards_dict.keys():
         one_shard_list = []
         for host in shards_dict[ip_addr]["click_containers"]:
             one_shard_list.append(host)
         distr_structure_list.append(one_shard_list)
 
+    # Added full_distr_cluster for config-file
+    for structure in distr_structure_list:
+        for replica in structure:
+            if not full_distr_cluster.get(replica[0], False):
+                full_distr_cluster[replica[0]] = []
+            full_distr_cluster[replica[0]].append(replica[1])
+
     inventory = {
         "clickhouse": {
             "hosts": list(shards_dict.keys()),
             "vars": {
                 "distr_structure": distr_structure_list,
-                "zoo_structure": zoo_structure_list
+                "zoo_structure": zoo_structure_list,
+                "full_distr_cluster": full_distr_cluster
             }
         },
         "_meta": {
@@ -80,8 +98,4 @@ def click_inventory(click_cluster):
         }
     }
 
-    #return(zoo_structure_list)
-    #return(distr_structure_list)
-    #return(shards_dict)
     print(convert_to_json(inventory))
-    #return(inventory)
